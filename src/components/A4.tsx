8561889b6397e3168fc473862207263dbf45a360
004Anuj <004Anuj>
2024-07-04 20:28:07 +0000

    Pushed from Locofy Builder

---
+++ src/components/A4.tsx
@@ -0,0 +1,301 @@
+import PropTypes from "prop-types";
+import styles from "./A4.module.css";
+
+const A4 = ({ className = "" }) => {
+  return (
+    <div className={[styles.a41, className].join(" ")}>
+      <div className={styles.profile}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.frameChild} />
+          <div className={styles.anujSahuParent}>
+            <div className={styles.anujSahu}>
+              <span>{` `}</span>
+              <span className={styles.span}>{`   `}</span>
+              <span className={styles.anujSahu1}>Anuj Sahu</span>
+            </div>
+            <div className={styles.fullStackDeveloperWrapper}>
+              <div className={styles.fullStackDeveloper}>
+                Full stack Developer
+              </div>
+            </div>
+          </div>
+          <div className={styles.phone8817577590EmailOfficiaWrapper}>
+            <div className={styles.phone8817577590EmailContainer}>
+              <p className={styles.phone}>
+                <span>Phone</span>
+              </p>
+              <p className={styles.p}>
+                <span>8817577590</span>
+              </p>
+              <p className={styles.email}>
+                <span>Email</span>
+              </p>
+              <p className={styles.officialanuj004gmailcom}>
+                <a
+                  className={styles.officialanuj004gmailcom1}
+                  href="mailto:officialanuj004@gmail.com"
+                  target="_blank"
+                >
+                  <span>
+                    <span className={styles.officialanuj004gmailcom2}>
+                      officialanuj004@gmail.com
+                    </span>
+                  </span>
+                </a>
+              </p>
+              <p className={styles.linkedin}>
+                <span>Linkedin:</span>
+              </p>
+              <p className={styles.wwwlinkedincominanujSahu}>
+                <a
+                  className={styles.wwwlinkedincominanujSahu1}
+                  href="https://www.linkedin.com/in/anuj-sahu-4059bb253"
+                  target="_blank"
+                >
+                  <span>
+                    <span className={styles.wwwlinkedincominanujSahu2}>
+                      www.linkedin.com/in/anuj-sahu-4059bb253
+                    </span>
+                  </span>
+                </a>
+              </p>
+              <p className={styles.github}>
+                <b>GitHub:</b>
+              </p>
+              <p className={styles.httpsgithubcomofficialanu}>
+                <a
+                  className={styles.httpsgithubcomofficialanu1}
+                  href="https://github.com/Officialanuj01"
+                  target="_blank"
+                >
+                  <span>
+                    <span className={styles.httpsgithubcomofficialanu2}>
+                      https://github.com/Officialanuj01
+                    </span>
+                  </span>
+                </a>
+              </p>
+              <p className={styles.httpsgithubcom004anuj}>
+                <span>https://github.com/004Anuj</span>
+              </p>
+              <p className={styles.blankLine}>
+                <span>&nbsp;</span>
+              </p>
+              <p className={styles.blankLine1}>
+                <span>&nbsp;</span>
+              </p>
+            </div>
+          </div>
+          <div className={styles.techSkillsFrontEndSkillsWrapper}>
+            <b className={styles.techSkillsFrontEndContainer}>
+              <p className={styles.techSkills}>Tech Skills:</p>
+              <p className={styles.frontEndSkillsHtml}>
+                Front-End Skills: HTML, CSS, Java Script,React
+              </p>
+              <p className={styles.backEndSkillsNode}>
+                Back-End Skills: Node js, Express, MongoDb
+              </p>
+              <p className={styles.programmingLanguagesPython}>
+                Programming languages: Python , Java
+              </p>
+              <p className={styles.gitVersionControl}>Git Version Control</p>
+              <p className={styles.responsiveDesign}>Responsive design</p>
+              <p className={styles.softSkills}>Soft Skills:</p>
+              <ul className={styles.timeManagementAbilitiesProf}>
+                <li className={styles.timeManagementAbilities}>
+                  Time management abilities
+                </li>
+                <li className={styles.professionalism}>Professionalism</li>
+                <li className={styles.hardWork}>Hard Work</li>
+                <li className={styles.teamWork}>Team work</li>
+                <li className={styles.communication}>Communication</li>
+                <li>Quick learner</li>
+              </ul>
+            </b>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentChild} />
+          <a className={styles.contact}>Contact</a>
+        </div>
+        <div className={styles.frameParent}>
+          <div className={styles.rectangleWrapper}>
+            <div className={styles.frameItem} />
+          </div>
+          <b className={styles.skills}>Skills</b>
+        </div>
+      </div>
+      <div className={styles.contentColumns} />
+      <div className={styles.contentColumns1} />
+      <section className={styles.frameGroup}>
+        <div className={styles.organizedAndDependableCompuWrapper}>
+          <div className={styles.organizedAndDependable}>
+            Organized and dependable computer science student with a passion for
+            learning and a positive attitude. Skilled at managing multiple
+            priorities and always eager to take on new challenges to expand my
+            knowledge and skills in technology. Humble and adaptable, with a
+            constant desire to grow and improve.
+          </div>
+        </div>
+        <div className={styles.frameContainer}>
+          <div className={styles.rectangleGroup}>
+            <div className={styles.frameInner} />
+            <b className={styles.education}>Education</b>
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.softwareProductEngineeringWrapper}>
+            <div className={styles.softwareProductEngineeringContainer}>
+              <p className={styles.softwareProductEngineering}>
+                <span className={styles.span1}>
+                  <span
+                    className={styles.span2}
+                  >{`2023-04 -            `}</span>
+                </span>
+                <span>
+                  <span className={styles.span3}> </span>
+                  <b>Software Product Engineering (Kalvium)</b>
+                </span>
+              </p>
+              <p className={styles.jecrcUniversityJaipurIndi}>
+                <span>
+                  <span className={styles.jecrcUniversity}>
+                    2027-04    Jecrc University - Jaipur India
+                  </span>
+                </span>
+              </p>
+              <p className={styles.cgpa92}>
+                <span>
+                  <span className={styles.cgpa921}>             CGPA 9.2</span>
+                </span>
+              </p>
+              <p className={styles.blankLine2}>
+                <span>
+                  <span className={styles.blankLine3}>&nbsp;</span>
+                </span>
+              </p>
+              <p className={styles.sciencepcm}>
+                <span>
+                  <span className={styles.span4}>{`2021-04 `}</span>
+                  <b className={styles.b}>{`              `}</b>
+                </span>
+                <b className={styles.sciencepcm1}>
+                  <span className={styles.sciencepcm2}>Science/PCM</span>
+                </b>
+              </p>
+              <p className={styles.sLMemorial}>
+                {" "}
+                S L Memorial Higher Secondry School - Guna
+              </p>
+              <p className={styles.percentage914}> Percentage 91.4</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.frameChild1} />
+          <b className={styles.participationInHackathons}>
+            Participation in Hackathons
+          </b>
+        </div>
+        <div className={styles.cricketCodeChampionBuildAWrapper}>
+          <div className={styles.cricketCodeChampionContainer}>
+            <p className={styles.cricketCodeChampion}>
+              <b>Cricket- Code Champion</b>
+            </p>
+            <p className={styles.buildAPrototype}>
+              build a prototype to increase Fan experience in the cricket.
+            </p>
+            <p className={styles.teamLeaderIn}>
+              Team Leader in a team of 5 receiving special mention among 1700
+              registrations.
+            </p>
+            <p className={styles.devOn}>
+              <b>Dev-ON -</b>
+            </p>
+            <p className={styles.weHadSubmitted}>
+              We had submitted an idea about how we get the Real-time data of
+              our population
+            </p>
+            <p className={styles.strategyTrack}>
+              <b>Strategy Track-</b>
+            </p>
+            <p className={styles.iHaveSubmitted}>
+              I have submitted an idea of how we would make Dishwashers a
+              household necessity
+            </p>
+          </div>
+        </div>
+        <div className={styles.frameDiv}>
+          <div className={styles.rectangleParent1}>
+            <div className={styles.frameChild2} />
+            <b className={styles.projects}>Projects</b>
+          </div>
+          <div className={styles.matchingGameDevelopedContainer}>
+            <p className={styles.matchingGame}>
+              <b className={styles.matchingGame1}>Matching Game</b>
+            </p>
+            <p className={styles.developedAPairMatchingGame}>
+              <span className={styles.developedAPairMatchingGame1}>
+                • Developed a pair-matching game using HTML, CSS and JavaScript
+              </span>
+            </p>
+            <p className={styles.deployedlinkHttpsgameD}>
+              <span className={styles.deployedlink}>•  deployedLink = [</span>
+              <a
+                className={styles.httpsgameDevelopmentCa1}
+                href="https://game-development-ca-1.vercel.app/"
+                target="_blank"
+              >
+                <span className={styles.httpsgameDevelopmentCa11}>
+                  <span className={styles.httpsgameDevelopmentCa12}>
+                    https://game-development-ca-1.vercel.app/
+                  </span>
+                </span>
+              </a>
+              <span className={styles.span5}>]</span>
+            </p>
+            <p className={styles.uniqueStories}>
+              <b className={styles.uniqueStories1}>Unique Stories</b>
+            </p>
+            <p className={styles.aUserCanBrowseAndUploadT}>
+              <span className={styles.aUserCanBrowseAndUploadT1}>
+                • A user can Browse and upload their unique stories
+              </span>
+            </p>
+            <p className={styles.useMernStackToBuildIt}>
+              <span className={styles.useMernStackToBuildIt1}>
+                •  Use MERN stack to build it.
+              </span>
+            </p>
+            <p className={styles.deployedlinkHttpsmain}>
+              <span className={styles.deployedlink2}> deployedLink = [</span>
+              <a
+                className={styles.httpsmainUniquestonetlif}
+                href="https://main--uniquesto.netlify.app/"
+                target="_blank"
+              >
+                <span className={styles.httpsmainUniquestonetlif1}>
+                  <span className={styles.httpsmainUniquestonetlif2}>
+                    https://main--uniquesto.netlify.app/
+                  </span>
+                </span>
+              </a>
+              <span className={styles.span6}>{`] `}</span>
+            </p>
+            <p className={styles.blankLine4}>
+              <b className={styles.blankLine5}>&nbsp;</b>
+            </p>
+            <p className={styles.p1}>
+              <b className={styles.b1}> </b>
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+A4.propTypes = {
+  className: PropTypes.string,
+};
+
+export default A4;
